8e29df792d20a8ba62cf984c4a0b04aaca30f3a1
dev-girii <giridharan.r.2023.cce@ritchennai.edu.in>
2025-08-09 18:47:05 +0530

    Updated Code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
-$(document).ready(function() {
-    // hide the text first
-    $(".typewriter h1").hide();
-  
-    // show the text with typewriter effect
-    setTimeout(function(){
-      $(".typewriter h1").show();
-    }, 1500);
-  
-  });
-  
+$(document).ready(function(){$(".typewriter h1").hide();setTimeout(function(){$(".typewriter h1").show()},1500)})
